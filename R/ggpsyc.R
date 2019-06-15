856ad591f38a119ff0934a188f0667647b78f656
David Stanley <dstanley@uoguelph.ca>
2019-06-15 11:47:58 -0400

    initial commit with working code

---
+++ R/ggpsyc.R
@@ -0,0 +1,25 @@
+#' Users of the Introduction to the New Statistics textbook may wish to create graphs similar to those in the book - which this package faciliates.
+#'
+#' Bugs and feature requests can be reported at: \url{https://github.com/dstanley4/ggpsyc/issues}
+#'
+#'\tabular{ll}{
+#'Package: \tab ggpsyc\cr
+#'Type: \tab Package\cr
+#'Version: \tab 0.0.9\cr
+#'Date: \tab 2019-06-15\cr
+#'}
+#'
+#'@name apaTables
+#'@aliases ggpsyc
+#'@docType package
+#'@title Greate ITNS graphs
+#'@author
+#'\tabular{ll}{
+#'Author: \tab David J. Stanley \email{dstanley@@uoguelph.ca}\cr
+#'Maintainer: \tab David J. Stanley \email{dstanley@@uoguelph.ca}
+#'}
+#'@importFrom "stats" "qnorm" "rnorm" "lm"
+utils::globalVariables(c(".data"))
+NULL
+
+
